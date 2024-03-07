4f3e90c5ddd75ea6aa711fa15b008b97a8e37f0f
luka-danger <lukathevegan@gmail.com>
2024-03-07 11:51:52 -0700

    Create About Page Controller

---
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    public String index() {
+        return "about";
+    }
+}
+
+
+
+
