f860686704ca361e4bb1fbc802b04885731215fc
luka-danger <lukathevegan@gmail.com>
2024-03-07 14:48:42 -0700

    Refactor Home Page and Controller
    Change home page and home controller to about. ADD CHANGE: Create a separate about page for guest account

--- src/main/java/com/example/demo/controllers/AboutController.java
+++ src/main/java/com/example/demo/controllers/AboutController.java
@@ -5,13 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AboutController {
-
     @RequestMapping("/about")
     public String index() {
         return "about";
     }
 }
-
-
-
-
