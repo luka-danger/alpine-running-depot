5ef1e4517beae44b6317fd278d8b56ff14123c44
luka-danger <lukathevegan@gmail.com>
2024-03-12 10:27:47 -0600

    Add Five Replacement Parts and Products to Inventory
    Create 5 new Part objects
    Write if statement to add new objects to the parts repo if there are no existing objects in the parts repo
    Create 5 new Product objects
    Write if statement to add new objects to the products repo if there are no existing objects in the products repo
    Make Part Object Constructor a public class that can be instantiated
    Comment out abstract class in Part Object Constructor
    Comment out Outsource Part sections in BootStrap Data as Outsourced parts were unused on this app
    Update media query on inventory / main to change display when window is less than 800px

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,9 +1,11 @@
 package com.example.demo.bootstrap;
 
-import com.example.demo.repositories.OutsourcedPartRepository;
+// import com.example.demo.domain.OutsourcedPart;
+// import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
-import com.example.demo.domain.OutsourcedPart;
+import com.example.demo.domain.Part;
+import com.example.demo.domain.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -21,18 +23,19 @@ public class BootStrapData implements CommandLineRunner {
     private final PartRepository partRepository;
     private final ProductRepository productRepository;
 
-    private final OutsourcedPartRepository outsourcedPartRepository;
+    // private final OutsourcedPartRepository outsourcedPartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
-        this.outsourcedPartRepository=outsourcedPartRepository;
+        // this.outsourcedPartRepository=outsourcedPartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-       /*
+        // Unused Demo Code
+        /*
         OutsourcedPart o= new OutsourcedPart();
         o.setCompanyName("Western Governors University");
         o.setName("out test");
@@ -47,12 +50,43 @@ public class BootStrapData implements CommandLineRunner {
         }
 
         System.out.println(thePart.getCompanyName());
-        */
         List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
         for(OutsourcedPart part:outsourcedParts){
             System.out.println(part.getName()+" "+part.getCompanyName());
         }
+        */
+        // Sample Product Inventory
+        Part shoelace = new Part("Shoelace Replacement", 4.99, 20);
+        Part vestStrap = new Part("Vest Strap Replacement", 8.99, 10);
+        Part headBand = new Part("Head Lamp Band Replacement", 14.99, 6);
+        Part lampBulb = new Part("Head Lamp Bulb Replacement", 16.99, 9);
+        Part filter = new Part("Replacement Water Filter", 15.99, 15);
 
+        if (partRepository.count() == 0) {
+            partRepository.save(shoelace);
+            partRepository.save(vestStrap);
+            partRepository.save(headBand);
+            partRepository.save(lampBulb);
+            partRepository.save(filter);
+        }
+
+        // Sample Product Inventory
+
+        Product hokas = new Product("Hoka Speedgoat 5", 149.99, 4);
+        Product wildhorse = new Product("Nike Wildhorse 8", 97.99, 10);
+        Product ultimateVest = new Product("Ultimate Direction Hydration Vest", 109.99, 5);
+        Product headLamp = new Product("Petzl Headlamp", 59.99, 6);
+        Product waterFilter = new Product("BeFree Filtered Water Bottle", 39.99, 8);
+
+        if (productRepository.count() == 0) {
+            productRepository.save(hokas);
+            productRepository.save(wildhorse);
+            productRepository.save(ultimateVest);
+            productRepository.save(headLamp);
+            productRepository.save(waterFilter);
+        }
+
+        // Unused Demo Code
         /*
         Product bicycle= new Product("bicycle",100.0,15);
         Product unicycle= new Product("unicycle",100.0,15);
@@ -60,10 +94,11 @@ public class BootStrapData implements CommandLineRunner {
         productRepository.save(unicycle);
         */
 
+
         System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
+        System.out.println("Number of Products: "+productRepository.count());
         System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
+        System.out.println("Number of Parts: "+partRepository.count());
         System.out.println(partRepository.findAll());
 
     }
