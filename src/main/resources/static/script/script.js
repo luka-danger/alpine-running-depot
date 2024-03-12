94c68b44c419d227d63f6991569055060a74f19b
luka-danger <lukathevegan@gmail.com>
2024-03-12 13:23:56 -0600

    Create noInventory() Custom Function
    Use Document Object Manipulation and For Loop to iterate through each products inventory,
    If the inventory for any product is equal to 0, change the font color of the product name and inventory number to red

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -10,3 +10,22 @@ function updateTableOverflow() {
 function displayAlert() {
     alert("This button doesn't work because it isn't a real website. It's just for school.");
 }
+
+function noInventory() {
+    let productInventory = document.getElementsByClassName('product-inventory');
+    for (let i = 0; i < productInventory.length; i++) {
+        let inventory = parseInt(productInventory[i].innerText.trim());
+        let productName = productInventory[i].parentNode.querySelector('.product-name');
+        if (inventory === 0) {
+            productName.style.color = "red";
+            productInventory[i].style.color= "red";
+        }
+    }
+}
+
+window.onload = function() {
+    noInventory();
+    updateTableOverflow();
+};
+
+
